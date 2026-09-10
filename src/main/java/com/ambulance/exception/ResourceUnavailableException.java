371150104af3aaeadb890b253ebc54eb9de91f0d
Genziitian <genziitian@gmail.com>
2026-09-10 14:20:04 +0530

    Add ResourceUnavailableException class

---
+++ src/main/java/com/ambulance/exception/ResourceUnavailableException.java
@@ -0,0 +1,7 @@
+package com.ambulance.exception;
+
+public class ResourceUnavailableException extends RuntimeException {
+    public ResourceUnavailableException(String message) {
+        super(message);
+    }
+}
